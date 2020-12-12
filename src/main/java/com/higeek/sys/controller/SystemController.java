90d998d47de9256eb0fffec5fdc8a8c5e12b36ab
Gentleman <670581679@qq.com>
2020-12-12 21:30:02 +0800

    更新温度计

--- src/main/java/com/higeek/sys/controller/SystemController.java
+++ src/main/java/com/higeek/sys/controller/SystemController.java
@@ -3,12 +3,31 @@ package com.higeek.sys.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-@RequestMapping("sys")
+@RequestMapping("")
 public class SystemController {
 
-    @RequestMapping("index")
-    public String index(){
-        return "system/index/index";
+    @RequestMapping("")
+    public String index(HttpServletRequest httpServletRequest) {
+        String header = httpServletRequest.getHeader("user-agent");
+        System.out.println("sssss"+header);
+        if (header.indexOf("Android") != -1) {
+
+            return "system/index/index1";
+
+        } else if (header.indexOf("iPhone") != -1 || header.indexOf("iPad") != -1) {
+            return "system/index/index1";
+
+
+
+        } else {   // 电脑
+            return "system/index/index";
+
+      }
+
+
+
     }
 }
